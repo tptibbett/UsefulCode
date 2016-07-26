88ff2aa871176f2632ee19e7b287fae512159d85
Tom Tibbett <tptibbett@gmail.com>
2016-07-26 09:50:51 -0700

    Eliminated observations and concatenated

---
+++ DataMunging/EliminatingObsLoop.py
@@ -0,0 +1,35 @@
+# Importing relevant libraries
+import numpy as np
+import pandas as pd
+import glob
+import os
+
+filelist= [] # Blank lists we'll reference later.
+nonext=[]
+
+# Change directories to the relevant ones.
+my_dir = "C:\Users\I856176\Documents\Python Scripts\IAPS"
+output = "C:\Users\I856176\Documents\Python Scripts\IAPS\Output"
+os.chdir( my_dir )
+
+# Creating lists for the loop to reference.
+for files in glob.glob("*.dat"):
+    fileName, fileExtension = os.path.splitext(files)
+    nonext.append(fileName) #filename without extension
+    filelist.append(files) #filename with extension
+
+excl=[3, 25, 26, 33, 36, 44, 45, 46, 52, 54, 72, 82, 86, 87, 88, 90, 91, 92, 93, 97] # Picture Numbers to be excluded
+print 'Observations to be removed:'
+print excl, '\n'
+excl[:] = [x - 2 for x in excl] # Correcting for Python's indexing
+
+print 'Now Writing to Output Folder...'
+for i in filelist:
+    data=pd.read_csv(i, header=None, sep='\s+')
+    data=data.sort_values(9, ascending=True).reset_index(drop='index') # Sorting so that this is easier.
+    data=data.drop(data[9][excl]) # Drops the pictures we do not want.
+    #Printing to another folder because we do not want the files overriden.
+    #Ideally, this should be an empty folder beforehand.
+    data.to_csv(str(output) + str("\\") + i, header=None, index=False, sep='\t') 
+    print i, '\t\tWritten'
+print 'Complete!'
---
+++ DataMunging/MultiFileConcat.py
@@ -0,0 +1,31 @@
+# Multiple File Concatenation
+# Date of Creation: 07/25/2016
+# Created by Tom Tibbett
+
+import os # to make things non-specific to OS.
+import numpy as np            
+import pandas as pd
+import glob 
+
+# Remember, this is specifically for files that have the same columns.  They need to all be in the same folder.
+path = r'C:\\Users\\I856176\\Documents\\Python Scripts\\IAPS\\Output'
+
+# Be very careful! If you don't have your output in a different place, it will concatenate with the other files if you run this alone!
+output= r'C:\\Users\\I856176\\Documents\\Python Scripts\\IAPS\\Output\\IAPSimages.csv'
+all_files = glob.glob(os.path.join(path, "*.dat")) # Selects all .dat files in this path.  Make sure you want to concat all .dat files!
+
+# Concatenates all files into one pandas DataFrame.
+df = pd.concat(pd.read_csv(f, header=None, 
+                           index_col=False, usecols=[0,1,2,3,4,5,6,7,8,9], sep='\t',
+                          names=['Col_1', 'Col_2', 'Col_3', 'Col_4', 'Col_5', 'Col_6', 'Col_7', 'Col_8', 'Col_9', 'IAPSPic']) 
+               for f in all_files) 
+
+# What does our data set look like?
+print len(all_files), 'files to be concatenated, with', len(df)/len(all_files), 'observations per file.\n'
+print 'Preview...\n'
+print df.head(9)
+print '\nDataFrame Dimensions:'
+df.shape
+
+# Printing amalgamated file to a singular CSV file.
+df.to_csv(output, index=False, header=False)
